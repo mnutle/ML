e57db451a407407b34a8602536053b2f8bb1ab3d
mnutle <1166105051203@mail.rmutt.ac.th>
2026-02-25 10:32:30 +0700

    Add files via upload

--- multiple_classification.py
+++ multiple_classification.py
@@ -10,14 +10,15 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 #import model
-loan_model = pickle.load(open("C:/Users/Lab/Desktop/ML/loan_model.sav",'rb'))
-ridingmower_model = pickle.load(open("C:/Users/Lab/Desktop/ML/RidingMowers_model.sav",'rb'))
+loan_model = pickle.load(open("loan_model.sav",'rb'))
+ridingmower_model = pickle.load(open("RidingMowers_model.sav",'rb'))
+stress_model = pickle.load(open("stress_model.sav",'rb'))
 
 
 with st.sidebar:
-    selected = option_menu('Classification',['LOAN','RidingMower'])
+    selected = option_menu('Classification',['LOAN','RidingMower','Stress']) 
 
-if(selected == 'RidingMower'):
+    if(selected == 'LOAN'):
     st.title('RidingMower Prediction')
     
     #user input
@@ -39,48 +40,66 @@ if(selected == 'RidingMower'):
           RidingMower_predict = 'Owner'
     st.success(RidingMower_predict)
 
-   
-if(selected == 'LOAN'):
-    st.title('Loan Prediction')
+    if(selected == 'RidingMower'):
+    st.title('RidingMower Prediction')
     
     #user input
-    person_age = st.text_input('person_age')
-    person_gender = st.text_input('person_gender')
-    person_education = st.text_input('person_education')
-    person_income = st.text_input('person_income')
-    person_emp_exp = st.text_input('person_emp_exp')
-    person_home_ownership = st.text_input('person_home_ownership')
-    loan_amnt = st.text_input('loan_amnt')
-    loan_intent = st.text_input('loan_intent')
-    loan_int_rate = st.text_input('loan_int_rate')
-    loan_percent_income = st.text_input('loan_percent_income')
-    cb_person_cred_hist_length = st.text_input('cb_person_cred_hist_length')
-    credit_score = st.text_input('credit_score')
-    previous_loan_defaults_on_file = st.text_input('previous_loan_defaults_on_file')
+    Income = st.text_input('Income')
+    lotsize = st.text_input('lotsize')
+   
     
-    loan_predict = ''
+    RidingMower_predict = ''
     
     if st.button('Predict'):
-        loan_predict = loan_model.predict([[
-            float(person_age),
-            float(person_gender),
-            float(person_education),
-            float(person_income),
-            float(person_emp_exp),
-            float(person_home_ownership),
-            float(loan_amnt),
-            float(loan_intent),
-            float(loan_int_rate),
-            float(loan_percent_income),
-            float(cb_person_cred_hist_length),
-            float(credit_score),
-            float(previous_loan_defaults_on_file)
+        RidingMower_predict = ridingmower_model.predict([[
+            float(Income),
+            float(lotsize)
         ]])
         
-        if(loan_predict[0]==0):
-          loan_predict = 'Loan Reject'
+        if(RidingMower_predict[0]==0):
+          RidingMower_predict = 'Non Owner'
         else:
-          loan_predict = 'Loan Accept'
-    st.success(loan_predict)
+          RidingMower_predict = 'Owner'
+    st.success(RidingMower_predict)
+
+
+    if(selected == 'Stress'):
+    st.title('Stress Prediction')
+    
+    #user input
+    Age = st.text_input('Age')
+    Gender = st.text_input('Gender')
+    Occupation = st.text_input('Occupation')
+    Device_Type = st.text_input('Device_Type')
+    Daily_Phone_Hours = st.text_input('Daily_Phone_Hours')
+    Social_Media_Hours = st.text_input('Social_Media_Hours')
+    Work_Productivity_Score = st.text_input('Work_Productivity_Score')
+    Sleep_Hours = st.text_input('Sleep_Hours')
+    App_Usage_Count = st.text_input('App_Usage_Count')
+    Caffeine_Intake_Cups = st.text_input('Caffeine_Intake_Cups')
+    Weekend_Screen_Time_Hours = st.text_input('Weekend_Screen_Time_Hours')
+   
+    
+    stress_predict = ''
+    
+    if st.button('Predict'):
+        stress_predict = stress_model.predict([[
+            float(Age),
+            float(Gender),
+            float(Occupation),
+            float(Device_Type),
+            float(Daily_Phone_Hours),
+            float(Social_Media_Hours),
+            float(Work_Productivity_Score),
+            float(Sleep_Hours),
+            float(App_Usage_Count),
+            float(Caffeine_Intake_Cups),
+            float(Weekend_Screen_Time_Hours)
+        ]])
+        
+        
+    st.success(stress_predict)
+
+   
 
 
